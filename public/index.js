0aad7b10428df86d75c4ce9a106b6a89afe2418b
Vusithemba627@gmail.com <chickott@gmail.com>
2020-10-01 21:19:47 +0200

    index.js with api

--- public/index.js
+++ public/index.js
@@ -1,8 +1,18 @@
-let heading  = document.querySelector('h1')
-console.log(getData()  )
-
+let userName  = document.querySelector('username')
+let followers = document.querySelector("followers")
+let foowing = document.querySelector("following")
+// userName.textContent = getData().login
+getData()
 async function getData(){
-    let response = await fetch("https://api.github.com/users/techeraa");
+    let response = await fetch("http://api.github.com/users/techeraa");
     let data = await response.json();
-    return data.id;
+
+    document.getElementById("username").textContent += ` : ${data.login}`
+    document.getElementById("followers").textContent = data.followers
+    document.getElementById("following").textContent = data.following
+    document.getElementById("repositories").textContent = data.public_repos
+
+    console.log(data)
+
+
 }
